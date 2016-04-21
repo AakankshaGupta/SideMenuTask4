e3f99ab89f24e5de0415e9cdeb83c728316f7315
AakankshaGupta <gupta.aakanksha123@gmail.com>
2016-04-21 12:09:19 +0530

    Update SideMenuTask4Tests.swift

--- SideMenuTask4Tests/SideMenuTask4Tests.swift
+++ SideMenuTask4Tests/SideMenuTask4Tests.swift
@@ -24,6 +24,8 @@ class SideMenuTask4Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // bekar project.....
+        //:P
     }
     
     func testPerformanceExample() {
